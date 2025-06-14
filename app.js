797622a4dba32dbf6c0b78b4063da01e1e7e14ef
wpierce19 <126428398+wpierce19@users.noreply.github.com>
2025-06-14 14:24:09 -0600

    changed how app.js locates upload path

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
 import routes from "./routes/index.js";
 import passportConfig from "./middleware/passportConfig.js";
 
@@ -9,13 +10,16 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 //Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passportConfig.initialize());
-app.use("/uploads", express.static(path.resolve("uploads")));
+app.use("/uploads", express.static(resolve(__dirname, "uploads")));
 
 //Routes
 app.use(routes);
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 import { Router } from "express";
-import authRoutes from "./authRoutes";
-import friendRoutes from "./friendRoutes";
-import messageRoutes from "./messageRoutes";
-import profileRoutes from "./profileRoutes";
-import userRoutes from "./userRoutes";
+import authRoutes from "./authRoutes.js";
+import friendRoutes from "./friendRoutes.js";
+import messageRoutes from "./messageRoutes.js";
+import profileRoutes from "./profileRoutes.js";
+import userRoutes from "./userRoutes.js";
 
 const indexRouter = Router();
 
-indexRouter.use("/api", authRoutes);
-indexRouter.use("/api", friendRoutes);
-indexRouter.use("/api", messageRoutes);
-indexRouter.use("/api", profileRoutes);
-indexRouter.use("/api", userRoutes);
+indexRouter.use("/auth", authRoutes);
+indexRouter.use("/friends", friendRoutes);
+indexRouter.use("/messages", messageRoutes);
+indexRouter.use("/profile", profileRoutes);
+indexRouter.use("/users", userRoutes);
 
 export default indexRouter;
