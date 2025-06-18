53859692d31c39fb77985369c010cbfa516c2d1b
wpierce19 <126428398+wpierce19@users.noreply.github.com>
2025-06-17 18:32:46 -0600

    reverted

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passportConfig.initialize());
-app.use("/uploads", express.static("/mnt/data/uploads"));
+app.use("/uploads", express.static(resolve(__dirname, "uploads")));
 
 //Routes
 app.use(routes);
