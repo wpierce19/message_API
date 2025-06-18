b5c7a252bab30a6965f9b7ae036a6c33b5ef1c03
wpierce19 <126428398+wpierce19@users.noreply.github.com>
2025-06-17 18:30:08 -0600

    added path to persistant disk

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passportConfig.initialize());
-app.use("/uploads", express.static(resolve(__dirname, "uploads")));
+app.use("/uploads", express.static("/mnt/data/uploads"));
 
 //Routes
 app.use(routes);
