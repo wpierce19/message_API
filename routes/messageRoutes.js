092ed69aace768ad61a967d1727362c1adaec511
wpierce19 <126428398+wpierce19@users.noreply.github.com>
2025-06-20 18:21:28 -0600

    added new route

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -19,6 +19,7 @@ messageRouter.post("/", protect, upload.single("attachment"), createMessage);
 messageRouter.get("/:id", protect, getMessage);
 messageRouter.patch("/:id/read", protect, markAsRead);
 messageRouter.post("/:id/reply", protect, addReply);
-messageRouter.post("/:id/react/:commentId?", protect, reactToMessage);
+messageRouter.post("/:id/react", protect, reactToMessage);
+messageRouter.post("/:id/react/:commentId", protect, reactToMessage);
 
 export default messageRouter;
